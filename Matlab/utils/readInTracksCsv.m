5bd10ce8c962111b40ff9c26633b4f8115d31fbf
Robert Krajewski <robert.krajewski@ika.rwth-aachen.de>
2019-02-28 19:11:12 +0100

    Fix matlab csv track reader

--- Matlab/utils/readInTracksCsv.m
+++ Matlab/utils/readInTracksCsv.m
@@ -3,6 +3,10 @@ function [tracks] = readInTracksCsv(filename, filenameStatic)
 csvData = readtable(filename, 'Delimiter', ',');
 csvDataStatic = readtable(filenameStatic, 'Delimiter', ',');
 
+% Group by track id
+G = findgroups(csvData(:, 2));
+groupedById = splitapply(@(varargin)(varargin), csvData, G);
+
 % Build a map containing the track id and its corresponding index in the 
 % static tracks csv file. This enables fast access to the information when
 % creating the final tracks struct.
@@ -20,69 +24,56 @@ tracks = {};
 
 % Initialize constant variables
 trackIndex = 1;
-currentId = -1;
-trackLength = 0;
 
 % Iterate over the whole table
-for iRow = 1:size(csvData, 1)
-    iTrackId = csvData.id(iRow);
-    if currentId == -1
-        currentId = iTrackId;
-    end
-    % If the Id changes, the track has ended and it needs to be written 
-    % into the tracks struct 
-    if currentId ~= iTrackId
-        startingIndex = int32(iRow - trackLength);
-        tracks(trackIndex).id = currentId;
-        tracks(trackIndex).frames = csvData.frame(startingIndex:iRow);
-        tracks(trackIndex).bbox = [csvData.x(startingIndex:iRow) ...
-                                   csvData.y(startingIndex:iRow) ... 
-                                   csvData.width(startingIndex:iRow) ...
-                                   csvData.height(startingIndex:iRow)];
-        tracks(trackIndex).xVelocity = csvData.xVelocity(startingIndex:iRow);
-        tracks(trackIndex).yVelocity = csvData.yVelocity(startingIndex:iRow);
-        tracks(trackIndex).xAcceleration = csvData.xAcceleration(startingIndex:iRow);
-        tracks(trackIndex).yAcceleration = csvData.yAcceleration(startingIndex:iRow);
-        tracks(trackIndex).lane = csvData.laneId(startingIndex:iRow);
-        tracks(trackIndex).thw = csvData.thw(startingIndex:iRow);
-        tracks(trackIndex).ttc = csvData.ttc(startingIndex:iRow);
-        tracks(trackIndex).dhw = csvData.dhw(startingIndex:iRow);
-        tracks(trackIndex).frontSightDistance = csvData.frontSightDistance(startingIndex:iRow);
-        tracks(trackIndex).backSightDistance = csvData.backSightDistance(startingIndex:iRow);
-        tracks(trackIndex).precedingXVelocity = csvData.precedingXVelocity(startingIndex:iRow);
-        tracks(trackIndex).precedingId = csvData.precedingId(startingIndex:iRow);
-        tracks(trackIndex).followingId = csvData.followingId(startingIndex:iRow);
-        tracks(trackIndex).leftPrecedingId = csvData.leftPrecedingId(startingIndex:iRow);
-        tracks(trackIndex).leftAlongsideId = csvData.leftAlongsideId(startingIndex:iRow);
-        tracks(trackIndex).leftFollowingId = csvData.leftFollowingId(startingIndex:iRow);
-        tracks(trackIndex).rightPrecedingId = csvData.rightPrecedingId(startingIndex:iRow);
-        tracks(trackIndex).rightAlongsideId = csvData.rightAlongsideId(startingIndex:iRow);
-        tracks(trackIndex).rightFollowingId = csvData.rightFollowingId(startingIndex:iRow);
-        
-        csvStaticIndex = staticMap(currentId);
-        tracks(trackIndex).initialFrame = csvDataStatic.initialFrame(csvStaticIndex);
-        tracks(trackIndex).finalFrame = csvDataStatic.finalFrame(csvStaticIndex);
-        tracks(trackIndex).numFrames = csvDataStatic.numFrames(csvStaticIndex);
-        extractedClass = csvDataStatic.class(csvStaticIndex);
-        tracks(trackIndex).class = extractedClass{1};
-        tracks(trackIndex).minXVelocity = csvDataStatic.minXVelocity(csvStaticIndex);
-        tracks(trackIndex).maxXVelocity = csvDataStatic.maxXVelocity(csvStaticIndex);
-        tracks(trackIndex).meanXVelocity = csvDataStatic.meanXVelocity(csvStaticIndex);
-        tracks(trackIndex).drivingDirection = csvDataStatic.drivingDirection(csvStaticIndex);
-        tracks(trackIndex).traveledDistance = csvDataStatic.traveledDistance(csvStaticIndex);
-        tracks(trackIndex).minDHW = csvDataStatic.minDHW(csvStaticIndex);
-        tracks(trackIndex).minTHW = csvDataStatic.minTHW(csvStaticIndex);
-        tracks(trackIndex).minTTC = csvDataStatic.minTTC(csvStaticIndex);
-        tracks(trackIndex).numLaneChanges = csvDataStatic.numLaneChanges(csvStaticIndex);
+for iRow = 1:size(groupedById, 1)
+    iTrack = groupedById(iRow, :);
+    iTrackIds = cell2mat(iTrack(2));
+    currentId = iTrackIds(1);
+    
+    tracks(trackIndex).id = currentId;
+    tracks(trackIndex).frames = cell2mat(iTrack(1));
+    tracks(trackIndex).bbox = [cell2mat(iTrack(3)) ...
+                               cell2mat(iTrack(4)) ... 
+                               cell2mat(iTrack(5)) ...
+                               cell2mat(iTrack(6))];
+    tracks(trackIndex).xVelocity = cell2mat(iTrack(7));
+    tracks(trackIndex).yVelocity = cell2mat(iTrack(8));
+    tracks(trackIndex).xAcceleration = cell2mat(iTrack(9));
+    tracks(trackIndex).yAcceleration = cell2mat(iTrack(10));
+    tracks(trackIndex).lane = cell2mat(iTrack(25));
+    tracks(trackIndex).thw = cell2mat(iTrack(14));
+    tracks(trackIndex).ttc = cell2mat(iTrack(15));
+    tracks(trackIndex).dhw = cell2mat(iTrack(13));
+    tracks(trackIndex).frontSightDistance = cell2mat(iTrack(11));
+    tracks(trackIndex).backSightDistance = cell2mat(iTrack(12));
+    tracks(trackIndex).precedingXVelocity = cell2mat(iTrack(16));
+    tracks(trackIndex).precedingId = cell2mat(iTrack(17));
+    tracks(trackIndex).followingId = cell2mat(iTrack(18));
+    tracks(trackIndex).leftPrecedingId = cell2mat(iTrack(19));
+    tracks(trackIndex).leftAlongsideId = cell2mat(iTrack(20));
+    tracks(trackIndex).leftFollowingId = cell2mat(iTrack(21));
+    tracks(trackIndex).rightPrecedingId = cell2mat(iTrack(22));
+    tracks(trackIndex).rightAlongsideId = cell2mat(iTrack(23));
+    tracks(trackIndex).rightFollowingId = cell2mat(iTrack(24));
 
-      
-        % Reset the values again for next track
-        currentId = iTrackId;
-        
-        % Increment the internal track index 
-        trackIndex = trackIndex + 1;
-        trackLength = 0;
-    end
-    trackLength = trackLength + 1;
+    csvStaticIndex = staticMap(currentId);
+    tracks(trackIndex).initialFrame = csvDataStatic.initialFrame(csvStaticIndex);
+    tracks(trackIndex).finalFrame = csvDataStatic.finalFrame(csvStaticIndex);
+    tracks(trackIndex).numFrames = csvDataStatic.numFrames(csvStaticIndex);
+    extractedClass = csvDataStatic.class(csvStaticIndex);
+    tracks(trackIndex).class = extractedClass{1};
+    tracks(trackIndex).minXVelocity = csvDataStatic.minXVelocity(csvStaticIndex);
+    tracks(trackIndex).maxXVelocity = csvDataStatic.maxXVelocity(csvStaticIndex);
+    tracks(trackIndex).meanXVelocity = csvDataStatic.meanXVelocity(csvStaticIndex);
+    tracks(trackIndex).drivingDirection = csvDataStatic.drivingDirection(csvStaticIndex);
+    tracks(trackIndex).traveledDistance = csvDataStatic.traveledDistance(csvStaticIndex);
+    tracks(trackIndex).minDHW = csvDataStatic.minDHW(csvStaticIndex);
+    tracks(trackIndex).minTHW = csvDataStatic.minTHW(csvStaticIndex);
+    tracks(trackIndex).minTTC = csvDataStatic.minTTC(csvStaticIndex);
+    tracks(trackIndex).numLaneChanges = csvDataStatic.numLaneChanges(csvStaticIndex);
+    
+    % Increment the internal track index 
+    trackIndex = trackIndex + 1;
 end
 end
